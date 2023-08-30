59a3b7795cafce3aeeadbe7b5868640585ddfb21
Mathieu Cazenave <nunespace@gmail.com>
2023-08-31 00:26:53 +0200

    modif formulaire review avec Layout et Helper

--- review/forms.py
+++ review/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Ticket, Review, UserFollows
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout
+from crispy_forms.bootstrap import InlineRadios
 
 
 class TicketForm(forms.ModelForm):
@@ -12,12 +15,23 @@ class TicketForm(forms.ModelForm):
 
 
 class ReviewForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            'headline',
+            InlineRadios('rating'),
+            'body'
+        )
+
     class Meta:
         model = Review
         fields = ['headline', 'rating', 'body']
         widgets = {
             "headline": forms.TextInput(attrs={'placeholder': 'Titre de votre critique'}),
-            'rating': forms.RadioSelect(attrs={'aria-label': "Attribuez une note de 0 à 5"}, choices=((i, i) for i in range(0, 6)))
+            'rating': forms.RadioSelect(choices=((i, i) for i in range(0, 6)))
         }
 
 
